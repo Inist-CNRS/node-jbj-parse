e60eb57196165fcdc1ceedd50c260de68d34e5f4
François Parmentier <francois.parmentier@gmail.com>
2015-12-03 17:42:51 +0100

    :rotating_light: Add error testing.
    This test was located in node-jbj, but used jbj-parse.

---
+++ test/errorAsync.js
@@ -0,0 +1,34 @@
+/*jshint node:true,laxcomma:true*/
+/* global describe, it */
+'use strict';
+var assert = require('assert')
+  , JBJ = require('jbj');
+
+JBJ.use(require('..'));
+
+describe('asynchronous error', function () {
+
+  var input = {
+    "a" : {
+      "b" : {
+        "c" : "1234"
+      },
+      "d" : null
+    }
+  };
+
+  it('error #1', function(done) {
+    var stylesheet = {
+      "$a" : {
+        "default" : "\"xxxx",
+        "parseJSON" : true
+      }
+    };
+    JBJ.render(stylesheet, input, function (err, output) {
+      assert.equal(output.a.name, 'SyntaxError');
+      done(err);
+    });
+
+  });
+
+});
